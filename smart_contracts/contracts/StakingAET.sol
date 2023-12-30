40c1de10c7f48981de1ff3069ab49106be8f6c4d
ihsan215 <aliihsantas34@gmail.com>
2023-12-30 22:05:04 +0300

    edited stakingwallet.sol

--- smart_contracts/contracts/StakingAET.sol
+++ smart_contracts/contracts/StakingAET.sol
@@ -13,6 +13,7 @@ contract StakingAET is ERC20{
     event CreateWallet(uint256 indexed _walletId, address _address);
     event DepositWallet(uint256 indexed _walletId, uint256 amount);
     event WithdrawWallet(uint256 indexed _walletId, address _to, uint256 amount);
+    event StakingEthEvent(uint256 indexed _walletId, uint256 totalAmount);
 
 
     struct StakingWallet{
@@ -20,16 +21,19 @@ contract StakingAET is ERC20{
         uint256 stakedValue;
         uint256 sinceBlock;
         uint256 untilBlock;
+        uint256 totalReward;
         
     }
     
+    uint256 public constant rewardAmountPerBlock = 10;
+
     StakingWallet[] public stakingWallets;
     
     constructor() ERC20("AEON Token" , "AET"){}
 
     function createWallet() public returns(uint256,address){
         Wallet wallet = new Wallet();
-        stakingWallets.push(StakingWallet(wallet,0,0,0));
+        stakingWallets.push(StakingWallet(wallet,0,0,0,0));
         uint256 walletId = stakingWallets.length - 1;
         emit CreateWallet(walletId, address(wallet));
         return (walletId,address(wallet));
@@ -52,6 +56,40 @@ contract StakingAET is ERC20{
         StakingWallet storage wallet = stakingWallets[_walletId];
         return wallet.wallet.balanceOf();
     }
+
+    function calculateCurrentReward(uint256 _walletId) public view returns(uint256){
+        StakingWallet memory wallet = stakingWallets[_walletId];
+        uint256 stakedForBlocks = (block.timestamp - wallet.sinceBlock);
+        uint256 totalUnclaimedRewards = (wallet.stakedValue *
+            stakedForBlocks *
+            rewardAmountPerBlock) / 100;   
+        
+        return (wallet.totalReward+totalUnclaimedRewards);
+    }
+
+    function currentStake(uint256 _walletId) public view returns(uint256){
+      StakingWallet memory wallet = stakingWallets[_walletId];
+      return wallet.stakedValue;
+    }
+
+    function StakingEth(uint256 _walletId, uint256 stakedAmount) public {
+        StakingWallet storage wallet = stakingWallets[_walletId];
+        uint256 currentBalance = wallet.wallet.balanceOf();
+        require(currentBalance > stakedAmount, "Wallet Balance needs to be bigger stakedAmount");
+        wallet.wallet.withdraw(payable(address(this)) , stakedAmount);
+
+        uint256 totalUnclaimedRewards = calculateCurrentReward(_walletId);
+    
+        wallet.totalReward += totalUnclaimedRewards;
+        wallet.sinceBlock = block.timestamp;
+        wallet.untilBlock = 0;
+        wallet.stakedValue += stakedAmount;
+        emit StakingEthEvent( _walletId, wallet.stakedValue);
+    }
+
+
+
+
 
 
     receive() external payable {}
